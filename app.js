6ea28038d8e1b7d18446eced6a5ff20570884e27
Chris Qin <59617012+ChrisQinA00944299@users.noreply.github.com>
2020-10-08 18:26:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017");
+var nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/addname", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("<body style='background: lightblue; margin: auto; padding: 10px; width: 50%;'><p style='font-weight: bold; font-family: Helvetica;'>Name saved to database.</p></body>");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
